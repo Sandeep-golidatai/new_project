bfd10991eeb5d62924aa764a71804f2d334d9f25
sandeepGITS <you@example.com>
2024-10-17 15:09:55 +0530

    one_more_code

---
+++ one_more_code.py
@@ -0,0 +1,9 @@
+class Tenth():
+    def __init__(self,a,b,c):
+        self.a=a
+        self.b=b
+        self.c=c
+        print(self.a,self.b,self.c)
+u=Tenth('harish', 'suresh', 'haris')
+print(u)
+       
